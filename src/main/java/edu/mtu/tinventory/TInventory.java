0f423a5bff4676a28435c3cf9a31fd560e5a724a
Parker Young <parkery8283@gmail.com>
2017-10-09 14:44:44 -0400

    Added main class and FXML directory

---
+++ src/main/java/edu/mtu/tinventory/TInventory.java
@@ -0,0 +1,16 @@
+package edu.mtu.tinventory;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class TInventory extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
