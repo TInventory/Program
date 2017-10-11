c3366a465eea8c4aeec7ba96249103191c1fc1c9
JC Helm <jchelm@mtu.edu>
2017-10-11 14:57:47 -0400

    Setup basis of the configuration file for databases

--- src/main/java/edu/mtu/tinventory/database/DatabaseConfig.java
+++ src/main/java/edu/mtu/tinventory/database/DatabaseConfig.java
@@ -1,18 +1,50 @@
 package edu.mtu.tinventory.database;
 
+import java.io.File;
+import java.io.IOException;
+
 public class DatabaseConfig {
 
-	
-	private DatabaseConfig instance = new DatabaseConfig();
-	
-	
-	private DatabaseConfig() {
-		
+	private File file;
+
+	private static final DatabaseConfig sqlConfig = new DatabaseConfig("sqlConfig"), test = new DatabaseConfig("test");
+
+	/**
+	 * Constructor for config file class/classes
+	 * 
+	 * @param filename
+	 *            Filename of the class to be created
+	 */
+	private DatabaseConfig(String filename) {
+
+		// New instance of the filename
+		file = new File(filename);
+
+		// Try to create the new file
+		try {
+			file.createNewFile();
+		} 
+		// Catch exceptions
+		catch (IOException exception) {
+			System.out.println("Failed to create new file " + filename);
+			exception.getStackTrace();
+		}
 	}
-	
-	public DatabaseConfig getInstance() {
-		return instance;
+
+	/**
+	 * Saves the document 
+	 * 
+	 * @param file : file to be saved to the local datastore
+	 */
+	public boolean save(File file) {
+		try {
+
+		} catch (Exception exception) {
+			System.out.println("File " + file + " could not be saved");
+			exception.printStackTrace();
+			return false;
+		}
+		return true;
 	}
-	
-	
+
 }
