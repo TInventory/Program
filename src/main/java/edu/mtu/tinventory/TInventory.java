2fdcee0eab3cf4b48a8e51db21d051112bdcfd0b
Parker Young <parkery8283@gmail.com>
2017-10-11 14:38:13 -0400

    Added basic UI

--- src/main/java/edu/mtu/tinventory/TInventory.java
+++ src/main/java/edu/mtu/tinventory/TInventory.java
@@ -1,16 +1,32 @@
 package edu.mtu.tinventory;
 
+import edu.mtu.tinventory.gui.MainController;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 public class TInventory extends Application {
+	private MainController controller;
 
-    @Override
-    public void start(Stage stage) throws Exception {
+	@Override
+	public void start(Stage stage) throws Exception {
+		FXMLLoader loader = new FXMLLoader(TInventory.class.getResource("fxml/main.fxml"));
+		BorderPane root = loader.load();
+		controller = loader.getController();
+		Scene scene = new Scene(root);
 
-    }
+		stage.setTitle("TInventory");
+		stage.setScene(scene);
+		stage.show();
+	}
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+	public MainController getController() {
+		return controller;
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
---
+++ src/main/java/edu/mtu/tinventory/gui/MainController.java
@@ -0,0 +1,12 @@
+package edu.mtu.tinventory.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+
+/**
+ * Controller class for the main window of the program
+ */
+public class MainController {
+	@FXML
+	private BorderPane root;
+}
