da356d44aee169934d41797af98de45c69930b46
JC Helm <jchelm@mtu.edu>
2017-10-12 19:05:08 -0400

    Updated documentation on several of the methods.

--- src/main/java/edu/mtu/tinventory/database/Consumer.java
+++ src/main/java/edu/mtu/tinventory/database/Consumer.java
@@ -14,119 +14,133 @@ import edu.mtu.tinventory.database.query.Query;
  * @author James Helm
  * @since 10/12/2017
  * 
- *        Used to establish regular queue commands
- *        Likely will be used to auto update inventory
+ *        Used to establish regular queue commands Likely will be used to auto
+ *        update inventory
  */
 public class Consumer implements Runnable {
 
-    /**
-     * Instance of the database being accessed 
-     */
-    private Database db;
+	/**
+	 * Instance of the database being accessed
+	 */
+	private Database db;
 
-    /**
-     * Instance of this class, non sync here would be catastrophic
-     */
-    private static Consumer instance;
+	/**
+	 * Instance of this class, non sync here would be catastrophic
+	 */
+	private static Consumer instance;
 
-    /**
-     * Will need to be replaced with scheduler
-     */
-    // TODO: Replace with scheduler
-    // Remove suppression later, just used for now because it annoys me
-    @SuppressWarnings("unused")
-    private Object task;
+	/**
+	 * Will need to be replaced with scheduler
+	 */
+	// TODO: Replace with scheduler
+	// Remove suppression later, just used for now because it annoys me
+	@SuppressWarnings("unused")
+	private Object task;
 
-    private Queue<Query> queries;
+	private Queue<Query> queries;
 
-    /**
-     * Adds queries to a queue so that many can be done at once placing less
-     * stress on the database and increasing efficiency.
-     * 
-     * @param query
-     *            Query to add to the update queue
-     */
-    public static synchronized void queue(Query query) {
-        // check for instance existence
-        if (getInstance() != null) {
-            // add to query setup
-            getInstance().queries.add(query);
-        } else {
-            // maybe make some form of custom exception for here?
-            System.out.println("Error ---- ");
-        }
-    }
+	/**
+	 * Adds queries to a queue so that many can be done at once placing less
+	 * stress on the database and increasing efficiency.
+	 * 
+	 * @param query
+	 *            Query to add to the update queue
+	 */
+	public static synchronized void queue(Query query) {
+		// check for instance existence
+		if (getInstance() != null) {
+			// add to query setup
+			getInstance().queries.add(query);
+		} else {
+			// maybe make some form of custom exception for here?
+			System.out.println("Error ---- ");
+		}
+	}
 
-    /**
-     * Getter for class instance
-     * 
-     * @return Instance of Consumer
-     */
-    public static Consumer getInstance() {
-        return instance;
-    }
+	/**
+	 * Getter for class instance
+	 * 
+	 * @return Instance of Consumer
+	 */
+	public static Consumer getInstance() {
+		return instance;
+	}
 
-    /**
-     * 
-     * @param db
-     */
-    // TODO: docs
-    public Consumer(Database db) {
-        this.db = db;
-        instance = this;
+	/**
+	 * Class Constructor:
+	 * 
+	 * Creates the queue statement panel for database db
+	 * 
+	 * @param db
+	 *            Database to setup queue for
+	 */
+	public Consumer(Database db) {
+		// Link to class variable
+		this.db = db;
+		// assign this instance of the class as the local instance
+		instance = this;
 
-        this.queries = new LinkedBlockingQueue<Query>();
+		// Creates the Linked Queue to hold Statements
+		this.queries = new LinkedBlockingQueue<Query>();
 
-        // TODO: Need to replace with scheduler
-        this.task = null;
-    }
+		// TODO: Need to replace with task scheduler
+		this.task = null;
+	}
 
-    /**
-     * Stops the scheduler from running, stopping queues from being send
-     * automatically
-     */
-    public void stop() {
-        // TODO: Cancel the scheduler
-    }
+	/**
+	 * Stops the scheduler from running, stopping queues from being send
+	 * automatically
+	 */
+	public void stop() {
+		// TODO: Cancel the scheduler
+	}
 
-    /**
-     * 
-     */
-    // TODO: docs
-    @Override
-    public void run() {
-        if (!queries.isEmpty()) {
-            if (db.isConnected()) {
-                Query query;
-                while ((query = queries.poll()) != null) {
-                    // debug message
-                    // System.out.println(query.getQuery());
-                    try {
-                        if (query instanceof ExecuteQuery) {
-                            ExecuteQuery executeQuery = (ExecuteQuery) query;
+	/**
+	 * Takes all the Statements in the linked queue, and sends them to the database
+	 */
+	@Override
+	public void run() {
+		if (!queries.isEmpty()) {
+			if (db.isConnected()) {
+				Query query;
+				while ((query = queries.poll()) != null) {
+					// debug message
+					// System.out.println(query.getQuery());
+					try {
+						// Ensure the query is in a proper form
+						if (query instanceof ExecuteQuery) {
+							
+							ExecuteQuery executeQuery = (ExecuteQuery) query;
 
-                            Statement statement = db.createStatement();
-                            ResultSet set = statement.executeQuery(query.getQuery());
+							// Creates the statement to send
+							Statement statement = db.createStatement();
+							// Prepares the resultant set
+							ResultSet set = statement.executeQuery(query.getQuery());
 
-                            executeQuery.execute(set);
+							// Actually send out the statement
+							executeQuery.execute(set);
 
-                            set.close();
-                            statement.close();
+							// Close streams, to stop memory leaks
+							set.close();
+							statement.close();
 
-                        } else {
-                            String q = query.getQuery();
-                            if (q != null) {
-                                Statement statement = db.createStatement();
-                                statement.execute(q);
-                            }
-                        }
-                    }
-                    // catch the exceptions
-                    catch (SQLException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-    }
+						} else {
+							// Retrieve statement
+							String q = query.getQuery();
+							
+							if (q != null) {
+								Statement statement = db.createStatement();
+								// Send out the statement
+								statement.execute(q);
+							}
+						}
+					}
+					// catch the exceptions
+					catch (SQLException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}
+	}
 }
--- src/main/java/edu/mtu/tinventory/database/utils/DatabaseUtils.java
+++ src/main/java/edu/mtu/tinventory/database/utils/DatabaseUtils.java
@@ -24,202 +24,217 @@ import edu.mtu.tinventory.database.query.queries.InfoStreams;
  */
 public class DatabaseUtils {
 
-    private static final Pattern COMPILE = Pattern.compile("-", Pattern.LITERAL);
-    public static char[] allowed_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
+	private static final Pattern COMPILE = Pattern.compile("-", Pattern.LITERAL);
+	public static char[] allowed_chars = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 
-    /**
-     *  
-     * @return
-     */
-    //TODO: docs
-    public static InfoStreams newOutputStream() {
-        return new InfoStreams(new ByteArrayOutputStream());
-    }
+	/**
+	 * Creates a new blank instance of InfoStream
+	 * 
+	 * @return InfoStream : ByteArrayOutputStream : new blank instance
+	 */
+	public static InfoStreams newOutputStream() {
+		return new InfoStreams(new ByteArrayOutputStream());
+	}
 
-    /**
-     * 
-     * @param data
-     * @return
-     */
-    //TODO: docs
-    public static DataInputStream newInputStream(byte[] data) {
-        return new DataInputStream(new ByteArrayInputStream(data));
-    }
+	/**
+	 * Converts an array of bytes into a DataInputStream
+	 * 
+	 * @param data
+	 *            byte array to be converted
+	 * @return an instance of DataInputStream made from data
+	 */
+	public static DataInputStream newInputStream(byte[] data) {
+		return new DataInputStream(new ByteArrayInputStream(data));
+	}
 
-    /**
-     * Just here for testing
-     * 
-     * @param args
-     */
-    //TODO: docs
-    /*
-     * public static void main(String[] args) {
-     * System.out.println(getTimeString(128060230, true));
-     * }
-     */
+	/**
+	 * Just here for testing
+	 * 
+	 * @param args
+	 */
+	/*
+	 * public static void main(String[] args) {
+	 * System.out.println(getTimeString(128060230, true)); }
+	 */
 
-    /**
-     * 
-     * @param set
-     * @return
-     */
-    //TODO: docs
-    public static List<Object> getDataList(ResultSet set) {
-        List<Object> data = new ArrayList<>();
+	/**
+	 * Converts a ResultSet into a mutable List<Object>
+	 * 
+	 * @param set
+	 *            ResultSet to be converted in a List<Object>
+	 * @return An instance of List<Objects> of the ResultSet
+	 */
+	public static List<Object> getDataList(ResultSet set) {
+		List<Object> data = new ArrayList<>();
 
-        try {
-            if (set.first()) {
-                int count = set.getMetaData().getColumnCount();
-                for (int i = 1; i <= count; i++) {
-                    data.add(set.getObject(i));
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+		try {
+			if (set.first()) {
+				int count = set.getMetaData().getColumnCount();
+				for (int i = 1; i <= count; i++) {
+					data.add(set.getObject(i));
+				}
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 
-        return data;
-    }
+		return data;
+	}
 
-    /**
-     * 
-     * @param uuid
-     * @return
-     */
-    //TODO: docs
-    public static String strip(UUID uuid) {
-        return COMPILE.matcher(uuid.toString()).replaceAll("");
-    }
+	/**
+	 * Strips item of the hyphens in its UUID to become more workable, may not
+	 * be used depending on how we encode unique IDs
+	 * 
+	 * @param uuid
+	 *            Unique ID to be stripped of hyphens
+	 * @return Returns a String version of the UUID without hyphens
+	 */
+	public static String strip(UUID uuid) {
+		return COMPILE.matcher(uuid.toString()).replaceAll("");
+	}
 
-    /**
-     * 
-     * @param rs
-     * @return
-     * @throws SQLException
-     */
-    //TODO: docs
-    public static HashMap<String, Object> getData(ResultSet rs) throws SQLException {
-        ResultSetMetaData md = rs.getMetaData();
-        int columns = md.getColumnCount();
-        HashMap<String, Object> row = new HashMap<>();
-        while (rs.next()) {
-            for (int i = 1; i <= columns; i++) {
-                row.put(md.getColumnName(i), rs.getObject(i));
-            }
-        }
-        // System.out.println(row);
-        return row;
-    }
+	/**
+	 * Takes a resulting set and turns it into a very mutable and efficient
+	 * HashMap. Can be used with Product, but left open to allow for other data
+	 * stored.
+	 * 
+	 * @param resultingSet
+	 *            The resulting set to have data extracted from
+	 * @return Returns an instance of HashMap<String, Object>
+	 * @throws SQLException
+	 *             An exception that provides information on a database access
+	 *             error or other errors.
+	 */
+	public static HashMap<String, Object> getData(ResultSet resultingSet) throws SQLException {
+		// Grab the useful data of the resulting set
+		ResultSetMetaData meta = resultingSet.getMetaData();
+		int columns = meta.getColumnCount();
+		// Inner
+		HashMap<String, Object> row = new HashMap<String, Object>();
+		// Iterates through and inserts data into map
+		while (resultingSet.next()) {
+			for (int i = 1; i <= columns; i++) {
+				// Inserts the data with the String as the column name , and
+				// Object as the data under the column
+				row.put(meta.getColumnName(i), resultingSet.getObject(i));
+			}
+		}
+		// Return the hashmap with the data
+		return row;
+	}
 
-    /**
-     * 
-     * @param in
-     * @return
-     */
-    //TODO: docs
-    public static long getTime(final String in) {
-        long tot = 0;
-        final StringBuilder stored = new StringBuilder();
-        final char[] chars = in.toCharArray();
-        for (final char c : chars) {
-            boolean flag = false;
-            for (final char ch : DatabaseUtils.allowed_chars) {
-                if (ch == c) {
-                    flag = true;
-                    stored.append(ch);
-                    break;
-                }
-            }
-            if (!flag) {
-                final long t = DatabaseUtils.getTime(stored.toString(), c);
-                if (t == -1) {
-                    return -1;
-                }
-                tot += t;
-                stored.setLength(0);
-            }
+	/**
+	 * Converts timestamp strings to Unix millisecond time
+	 * 
+	 * @param in
+	 *            Timestamp to be converted
+	 * @return The timestamp in current milliseconds since the Unix Epoch
+	 */
+	public static long getTime(final String in) {
+		long total = 0;
+		final StringBuilder stored = new StringBuilder();
+		final char[] chars = in.toCharArray();
+		for (final char c : chars) {
+			boolean flag = false;
+			// Check for false data
+			for (final char ch : DatabaseUtils.allowed_chars) {
+				if (ch == c) {
+					flag = true;
+					stored.append(ch);
+					break;
+				}
+			}
+			// If data is true, handle it
+			if (!flag) {
+				final long time = DatabaseUtils.getTime(stored.toString(), c);
+				if (time == -1) {
+					return -1;
+				}
+				total += time;
+				stored.setLength(0);
+			}
 
-        }
+		}
 
-        return tot;
-    }
+		return total;
+	}
 
-    /**
-     * Retrieves the Unix time
-     * 
-     * @param in
-     *            String time to be parsed
-     * @param timescale
-     *            time scale to be used
-     * @return The long number of exact Unix time
-     */
-    private static long getTime(final String in, final char timescale) {
-        long tot = 0;
-        final int num = Integer.parseInt(in);
+	/**
+	 * Retrieves the Unix time
+	 * 
+	 * @param in
+	 *            String time to be parsed
+	 * @param timescale
+	 *            time scale to be used
+	 * @return The long number of exact Unix time
+	 */
+	private static long getTime(final String in, final char timescale) {
+		long tot = 0;
+		final int num = Integer.parseInt(in);
 
-        switch (timescale) {
-        case 's':
-            tot += num * 1000l;
-            break;
-        case 'm':
-            tot += num * 60000l;
-            break;
-        case 'h':
-            tot += num * 3600000l;
-            break;
-        case 'd':
-            tot += num * 86400000l;
-            break;
-        case 'w':
-            tot += num * 604800000l;
-            break;
-        default:
-            return -1;
-        }
-        return tot;
-    }
+		switch (timescale) {
+		case 's':
+			tot += num * 1000l;
+			break;
+		case 'm':
+			tot += num * 60000l;
+			break;
+		case 'h':
+			tot += num * 3600000l;
+			break;
+		case 'd':
+			tot += num * 86400000l;
+			break;
+		case 'w':
+			tot += num * 604800000l;
+			break;
+		default:
+			return -1;
+		}
+		return tot;
+	}
 
-    /**
-     * Creates and returns a simple time stamp
-     * 
-     * @param time
-     *            Unix time or from getTime
-     * @param getTruncatedStamps
-     *            if True uses shorthand terms for time stamp, if false uses
-     *            human recognizable terms
-     * @return A string time stamp in format of ("[days] Days [hours] Hours
-     *         [minutes] Minutes [seconds] Seconds") or their truncated form
-     */
-    public static String getTimeString(long time, boolean getTruncatedStamps) {
-        final StringBuilder str = new StringBuilder();
+	/**
+	 * Creates and returns a simple time stamp
+	 * 
+	 * @param time
+	 *            Unix time or from getTime
+	 * @param getTruncatedStamps
+	 *            if True uses shorthand terms for time stamp, if false uses
+	 *            human recognizable terms
+	 * @return A string time stamp in format of ("[days] Days [hours] Hours
+	 *         [minutes] Minutes [seconds] Seconds") or their truncated form
+	 */
+	public static String getTimeString(long time, boolean getTruncatedStamps) {
+		final StringBuilder str = new StringBuilder();
 
-        final long days = time / (1000 * 60 * 60 * 24);
-        time -= days * 1000 * 60 * 60 * 24;
-        final long hours = time / (1000 * 60 * 60);
-        time -= hours * 1000 * 60 * 60;
-        final long minutes = time / (1000 * 60);
-        time -= minutes * 1000 * 60;
-        final long seconds = time / 1000;
-        time -= seconds * 1000;
+		final long days = time / (1000 * 60 * 60 * 24);
+		time -= days * 1000 * 60 * 60 * 24;
+		final long hours = time / (1000 * 60 * 60);
+		time -= hours * 1000 * 60 * 60;
+		final long minutes = time / (1000 * 60);
+		time -= minutes * 1000 * 60;
+		final long seconds = time / 1000;
+		time -= seconds * 1000;
 
-        if (days > 0) {
-            // Uses "Days" if getTruncatedStamps is false, else use d
-            str.append(days).append(getTruncatedStamps ? " d " : "Days ");
-        }
-        if (hours > 0) {
-            // Uses "Hours" if getTruncatedStamps is false, else use h
-            str.append(hours).append(getTruncatedStamps ? " h " : "Hours ");
-        }
-        if (minutes > 0) {
-            // Uses "Minutes" if getTruncatedStamps is false, else use m
-            str.append(minutes).append(getTruncatedStamps ? " m " : " Minutes ");
-        }
-        if (seconds > 0) {
-            // Uses "Seconds" if getTruncatedStamps is false, else use s
-            str.append(seconds).append(getTruncatedStamps ? " s " : " Seconds ");
-        }
+		if (days > 0) {
+			// Uses "Days" if getTruncatedStamps is false, else use d
+			str.append(days).append(getTruncatedStamps ? " d " : "Days ");
+		}
+		if (hours > 0) {
+			// Uses "Hours" if getTruncatedStamps is false, else use h
+			str.append(hours).append(getTruncatedStamps ? " h " : "Hours ");
+		}
+		if (minutes > 0) {
+			// Uses "Minutes" if getTruncatedStamps is false, else use m
+			str.append(minutes).append(getTruncatedStamps ? " m " : " Minutes ");
+		}
+		if (seconds > 0) {
+			// Uses "Seconds" if getTruncatedStamps is false, else use s
+			str.append(seconds).append(getTruncatedStamps ? " s " : " Seconds ");
+		}
 
-        return str.toString();
-    }
+		return str.toString();
+	}
 
 }
