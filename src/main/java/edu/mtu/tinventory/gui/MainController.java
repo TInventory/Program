feb818408858022cd416dee6b54684ea19effe5a
Parker Young <parkery8283@gmail.com>
2017-10-13 14:43:57 -0400

    Added method for loading modular FXML files

--- src/main/java/edu/mtu/tinventory/gui/MainController.java
+++ src/main/java/edu/mtu/tinventory/gui/MainController.java
@@ -1,9 +1,14 @@
 package edu.mtu.tinventory.gui;
 
+import edu.mtu.tinventory.TInventory;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
 import javafx.scene.layout.BorderPane;
+
+import java.io.IOException;
 
 /**
  * Controller class for the main window of the program
@@ -18,12 +23,23 @@ public class MainController {
 	}
 
 	@FXML
-	public void viewInvetory() {
-		
+	public void viewInventory() {
+		try {
+			loadView("inventoryView.fxml");
+		} catch(IOException e) {
+			//TODO: Have a better error handler
+			e.printStackTrace();
+		}
 	}
 
 	@FXML
-	public void createNewProducts() {
+	public void createNewProduct() {
 		
+	}
+
+	private void loadView(String filename) throws IOException {
+		FXMLLoader loader = new FXMLLoader(TInventory.class.getResource("fxml/" + filename));
+		Node node = loader.load();
+		root.setCenter(node);
 	}
 }
