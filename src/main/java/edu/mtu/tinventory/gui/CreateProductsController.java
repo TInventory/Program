d9b4b54ec32df17ae32d3cfb6cfc5022ea60a72e
brandon <bjpaupor@mtu.edu>
2017-10-15 22:18:31 -0400

    Implemented a functional UI that creates a product from input

--- src/main/java/edu/mtu/tinventory/gui/CreateProductsController.java
+++ src/main/java/edu/mtu/tinventory/gui/CreateProductsController.java
@@ -1,7 +1,20 @@
 package edu.mtu.tinventory.gui;
 
+import edu.mtu.tinventory.data.Product;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 /**
  * Controller class for the product creation window
  */
 public class CreateProductsController {
+	@FXML TextField productName;
+	@FXML TextField productID;
+	@FXML TextField productPrice;
+	
+	@FXML
+	private void createProduct() {
+		Product product = new Product(productID.getText(), productName.getText(), productPrice.getText());
+		System.out.println(product);
+		
+	}
 }
