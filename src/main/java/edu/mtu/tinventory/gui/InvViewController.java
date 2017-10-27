ef39ebfc7d4871d494ed84e8eb0a5b8646bf6901
James Turkette <jturkett@c28-0117-06.ad.mtu.edu>
2017-10-27 14:57:34 -0400

    Changed invView xml, added some code to controller.

--- src/main/java/edu/mtu/tinventory/gui/InvViewController.java
+++ src/main/java/edu/mtu/tinventory/gui/InvViewController.java
@@ -1,13 +1,19 @@
 package edu.mtu.tinventory.gui;
 
 
+import java.util.List;
+
 import edu.mtu.tinventory.data.Product;
 import edu.mtu.tinventory.database.DatabaseInterface;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
 
 /**
  * Controller class for the inventory view
@@ -18,6 +24,8 @@ public class InvViewController {
 	@FXML private TableColumn<Product, String> idCol;
 	@FXML private TableColumn<Product, String> priceCol;
 	@FXML private TableColumn<Product, Number> qtyCol;
+	@FXML private TextField filter;
+	ObservableList<Product> list;
 	DatabaseInterface db;
 
 	/**Initializes the table, labels columns, gets any values the database may have.
@@ -29,7 +37,11 @@ public class InvViewController {
 		priceCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getDisplayPrice()));
 		qtyCol.setCellValueFactory(data -> new ReadOnlyIntegerWrapper(data.getValue().getQuanity().totalQty()));
 		db = DatabaseInterface.getInstance();
-		table.getItems().setAll(db.getProducts());      //Assuming local data is passed in a list
+		list = FXCollections.observableList(db.getProducts(null));
+		FilteredList<Product> filtered = new FilteredList<Product>(list, p -> true);
+																					//Input listener for textfield for changes, check order
+		table.getItems().setAll(db.getProducts(null));      //Assuming local data is passed in a list
+		table.setColumnResizePolicy((param) -> true);
 	}
 
 
@@ -39,7 +51,7 @@ public class InvViewController {
 	private void viewInventory() {
 		if (db != null) {               
 			db = DatabaseInterface.getInstance();
-			table.getItems().setAll(db.getProducts());
+			table.getItems().setAll(db.getProducts(null));
 		} else {
 			initialize();
 		}
