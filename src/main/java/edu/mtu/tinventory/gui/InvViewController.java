7ec22ee78edf76ef923294c2271a4e9794ee77d8
James Turkette <jturkett@c28-0117-06.ad.mtu.edu>
2017-10-30 14:13:17 -0400

    Fixed prior commit

--- src/main/java/edu/mtu/tinventory/gui/InvViewController.java
+++ src/main/java/edu/mtu/tinventory/gui/InvViewController.java
@@ -7,6 +7,7 @@ import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
@@ -50,6 +51,8 @@ public class InvViewController {
 				return false;
 			});
 		});
+	SortedList<Product> sort = new SortedList<>(filtered);
+	table.setItems(sort);	
 	}
 
 
