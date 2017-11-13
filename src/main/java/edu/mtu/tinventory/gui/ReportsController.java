31cca0a6d731f77cec090f22b6ad9e3c806c9fd7
James Turkette <jturkett@mtu.edu>
2017-11-13 14:47:46 -0500

    Reports now should push out CSV files

--- src/main/java/edu/mtu/tinventory/gui/ReportsController.java
+++ src/main/java/edu/mtu/tinventory/gui/ReportsController.java
@@ -2,6 +2,7 @@ package edu.mtu.tinventory.gui;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.Writer;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -17,54 +18,101 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
-public class ReportsController<P> extends Controller {
+public class ReportsController extends Controller {
 	@FXML private Button salesCSV;
 	@FXML private Button invCSV;
 	@FXML private Button custCSV;
 	private DatabaseInterface db = DatabaseInterface.getInstance();
 	
-	private void salesButton(ActionEvent event) {
-		List<Invoice> sales =  FXCollections.observableList(db.getInvoices());
-//		toCSV(1, sales);
-	}
-	
-	private void invButton(ActionEvent event) {
-		List<Product> inv = FXCollections.observableList(db.getProducts());
-//		toCSV(2, inv);
-	}
-	
-	private void custButton(ActionEvent event) {
-		List<Customer> cust =  FXCollections.observableList(db.getCustomers());
-//		toCSV(3, cust);
-	}
-	
-	private void toCSV (int typeTag, List<P> list) throws Exception {
-		String type = null;
-		
-		if (typeTag == 1) {
-			type = "Sales";
-		} else if (typeTag == 2) {
-			type = "Inventory";
-		} else {
-			type = "Customers";
-		}
+	private void salesButton(ActionEvent event) throws Exception {
+		List<Invoice> sales =  (List<Invoice>) FXCollections.observableList(db.getInvoices());
 		Writer write = null;
 		try {
 			SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
 			Date now = new Date(); 
-			File file = new File(type + sdf.format(now) + ".csv");
+			File file = new File("InventoryReport" + sdf.format(now) + ".csv");
 			write = new BufferedWriter(new FileWriter(file));
+			
+			for (Invoice sale: sales) {
+				String buff = sale.getCustomer() + "," + sale.getDate() + "," + sale.getProductsString() + "," + sale.getTotal() + "\n";
+				write.write(buff);
+			}
 			
 		} catch (Exception e) {
 			
 		} finally {
-			
+			write.flush();
+			write.close();
 		}
+
 	}
 	
+	private void invButton(ActionEvent event)  throws Exception {
+		List<Product> inv = (List<Product>) FXCollections.observableList(db.getProducts());
+		Writer write = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+			Date now = new Date(); 
+			File file = new File("InventoryReport" + sdf.format(now) + ".csv");
+			write = new BufferedWriter(new FileWriter(file));
+			
+			for (Product prod: inv) {
+				String buff = prod.getName() + "," + prod.getID() + "," + prod.getPrice() + "," + prod.getQuanity() + "\n";
+				write.write(buff);
+			}
+			
+		} catch (Exception e) {
+			
+		} finally {
+			write.flush();
+			write.close();
+		}
+
+	}
+	
+	private void custButton(ActionEvent event) throws Exception {
+		List<Customer> cust =  (List<Customer>) FXCollections.observableList(db.getCustomers());
+		Writer write = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+			Date now = new Date(); 
+			File file = new File("CustomerReport" + sdf.format(now) + ".csv");
+			write = new BufferedWriter(new FileWriter(file));
+			
+			for (Customer cus: cust) {
+				String buff = cus.getCompanyName() + "," + cus.getPersonName() + "," + cus.getPhoneNumber() + "," + cus.getAddress() + "," + cus.getFaxNumber() + "," + cus.getPastSales() + "\n";
+				write.write(buff);
+			}
+			
+		} catch (Exception e) {
+			
+		} finally {
+			write.flush();
+			write.close();
+		}
+	}	
+	
 	private void initialize() {
-		salesCSV.setOnAction(this::salesButton);
-		invCSV.setOnAction(this::invButton);
-		custCSV.setOnAction(this::custButton);
+		salesCSV.setOnAction(arg0 -> {
+			try {
+				salesButton(arg0);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		invCSV.setOnAction(event -> {
+			try {
+				invButton(event);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
+		custCSV.setOnAction(event -> {
+			try {
+				custButton(event);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		});
 	}
 }
