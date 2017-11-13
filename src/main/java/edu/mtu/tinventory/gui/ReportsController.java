9bce2cc89874d6b14ac0ab9dd7955735fd616b2a
Jturkett <jturkett@mtu.edu>
2017-11-13 03:47:33 -0500

    Added work on reports controller, midway through rewriting it after botching some of the structure of it. Will aim to fix after nap.

--- src/main/java/edu/mtu/tinventory/gui/ReportsController.java
+++ src/main/java/edu/mtu/tinventory/gui/ReportsController.java
@@ -1,7 +1,14 @@
 package edu.mtu.tinventory.gui;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 import edu.mtu.tinventory.data.Customer;
+import edu.mtu.tinventory.data.Invoice;
 import edu.mtu.tinventory.data.Product;
 import edu.mtu.tinventory.database.DatabaseInterface;
 import edu.mtu.tinventory.state.StateRegistry;
@@ -15,7 +22,7 @@ public class ReportsController extends Controller {
 	private DatabaseInterface db = DatabaseInterface.getInstance();
 	
 	private void salesButton() {
-		List<Product> sales;
+		List<Invoice> sales;
 	}
 	
 	private void invButton() {
@@ -24,5 +31,30 @@ public class ReportsController extends Controller {
 	
 	private void custButton() {
 		List<Customer> cust;
+	}
+	
+	private void toCSV (int typeTag) throws Exception {
+		String type = null;
+		
+		if (typeTag == 1) {
+			type = "Sales";
+		} else if (typeTag == 2) {
+			type = "Inventory";
+		} else {
+			type = "Customers";
+		}
+		Writer write = null;
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy_MM_dd_HH_mm_ss");
+			Date now = new Date(); 
+			File file = new File(type + sdf.format(now) + ".csv");
+			write = new BufferedWriter(new FileWriter(file));
+			
+		} catch (Exception e) {
+			
+			
+		} finally {
+			
+		}
 	}
 }
