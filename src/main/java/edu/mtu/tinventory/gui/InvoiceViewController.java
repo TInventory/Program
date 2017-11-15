290067f8a68e240f974b3e6f20c1ba91567e0efa
bjpaupor <bjpaupor@mtu.edu>
2017-11-15 14:33:16 -0500

    Moved reverting invoice into invoice view and removed confirm revert.

--- src/main/java/edu/mtu/tinventory/gui/InvoiceViewController.java
+++ src/main/java/edu/mtu/tinventory/gui/InvoiceViewController.java
@@ -1,7 +1,9 @@
 package edu.mtu.tinventory.gui;
 
 import edu.mtu.tinventory.data.Invoice;
+import edu.mtu.tinventory.data.PurchasedProduct;
 import edu.mtu.tinventory.database.DatabaseInterface;
+import edu.mtu.tinventory.state.StateRegistry;
 import javafx.beans.property.ReadOnlyIntegerWrapper;
 import javafx.beans.property.ReadOnlyStringWrapper;
 import javafx.collections.FXCollections;
@@ -9,16 +11,11 @@ import javafx.collections.ObservableList;
 import javafx.collections.transformation.FilteredList;
 import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
 /**
  * Controller class for the invoice view
@@ -34,7 +31,6 @@ public class InvoiceViewController extends Controller {
 	@FXML private Button revertInvoice;
 	private ObservableList<Invoice> list;
 	private DatabaseInterface db;
-	private Image icon;
 	
 	/**Initializes the table, labels columns, gets any values the database may have.
 	 * 
@@ -98,20 +94,20 @@ public class InvoiceViewController extends Controller {
 	}
 	
 	@FXML
-	public void revert() throws Exception {
-		icon = new IconLoader().getIcon();
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("fxml/revertInvoice.fxml"));
-		GridPane root = loader.load();
-		Stage stage = new Stage();
-		ConfirmRevertController c = loader.getController();
-		c.setMainApp(this.mainApp);
-		c.setStage(stage);
-		Invoice i = table.getSelectionModel().getSelectedItem();
-		stage.setTitle("Verify Revert Invoice #" + i.getId());
-		c.invoice = i;
-		stage.setScene(new Scene(root));
-		stage.getIcons().add(icon);
-		stage.showAndWait();
+	public void revert() {
+		Invoice invoice = table.getSelectionModel().getSelectedItem();
+		if (invoice == null) {
+			Dialogs.showDialog(Dialogs.Type.ERROR, null, "Please select an invoice to revert.");
+			return;
+		}
+		if (Dialogs.showYesNoDialog(Dialogs.Type.WARNING, "Confirm revert of invoice", "Are you certain you wish to revert invoice " + 
+				invoice.getId() + "?")) {
+			db.deleteInvoice(invoice); //remove from database
+			for (PurchasedProduct pp : invoice.getProducts()) { //return products to database
+				pp.getProduct().getQuanity().moveQty(StateRegistry.SOLD_STATE, StateRegistry.AVAILABLE_STATE, pp.getQuantity());
+				DatabaseInterface.getInstance().updateItem(pp.getProduct());
+			}
+		}
 		refresh();
 	}
 	
