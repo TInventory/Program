603b70b600e1f8201a6a60b59e5bb99670f60b98
bjpaupor <bjpaupor@mtu.edu>
2017-11-15 15:19:54 -0500

    Fixed update products to display in a decent looking way.

--- src/main/java/edu/mtu/tinventory/gui/UpdateProductsController.java
+++ src/main/java/edu/mtu/tinventory/gui/UpdateProductsController.java
@@ -1,42 +1,111 @@
 package edu.mtu.tinventory.gui;
 
-import edu.mtu.tinventory.TInventory;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import edu.mtu.tinventory.data.Product;
 import edu.mtu.tinventory.database.DatabaseInterface;
-import edu.mtu.tinventory.logging.LocalLog;
 import edu.mtu.tinventory.state.StateRegistry;
 import edu.mtu.tinventory.util.DatabaseUtils;
 import edu.mtu.tinventory.util.StringUtils;
+import javafx.beans.property.ReadOnlyIntegerWrapper;
+import javafx.beans.property.ReadOnlyStringWrapper;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
+import javafx.collections.transformation.SortedList;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
 
 /**
  * Controller class for the product creation window
  * @author Brandon Paupore
  */
 public class UpdateProductsController extends Controller {
-	DatabaseInterface database;
+	@FXML private TableView<Product> table;
+	@FXML private TableColumn<Product, String> nameCol;
+	@FXML private TableColumn<Product, String> idCol;
+	@FXML private TableColumn<Product, String> priceCol;
+	@FXML private TableColumn<Product, Number> qtyCol;
+	@FXML private TextField filter;
+	@FXML private TextField tagFilter;
+	private ObservableList<Product> list;
+	private DatabaseInterface db;
+	private ArrayList<String> tagList;
 
 	@FXML private TextField productID;
 	@FXML private TextField quantity;
-	@FXML private BorderPane borderPane;
-		  private VBox invView;
+	@FXML private Button button;
+		  
 	
 	@FXML
 	public void initialize() {
-		database = DatabaseInterface.getInstance();
-		try {
-			invView = new FXMLLoader(TInventory.class.getResource("fxml/inventoryView.fxml")).load();
-			borderPane.setLeft(invView);
-		}
-		catch (Exception e) {
-			LocalLog.exception(e);
-		}
+		nameCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getName()));
+		idCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getID()));
+		priceCol.setCellValueFactory(data -> new ReadOnlyStringWrapper(data.getValue().getDisplayPrice()));
+		qtyCol.setCellValueFactory(data -> new ReadOnlyIntegerWrapper(data.getValue().getQuanity().getQty(StateRegistry.AVAILABLE_STATE)));
+		db = DatabaseInterface.getInstance();
+		list = FXCollections.observableList(db.getProducts());
+		FilteredList<Product> filtered = new FilteredList<Product>(list, p -> true);
+		filter.textProperty().addListener((observable, oldV, newV) ->{ 
+			filtered.setPredicate((Product product) -> {
+				if (newV == null || newV.isEmpty()) {
+					return true;
+				}
+				String lowerCaseFilter = newV.toLowerCase();
+				if (product.getName().toLowerCase().contains(lowerCaseFilter)) {    //Extend to addition else ifs to add search areas. Convert fields to string
+					return true;
+				} else if (product.getID().toLowerCase().contains(lowerCaseFilter)) {
+					return true;
+				}
+				return false;
+			});
+		});
+
+		tagFilter.textProperty().addListener((observable, oldV, newV) ->{ 
+			if (tagFilter.getLength() > 0){
+				tagList =  new ArrayList<String>(Arrays.asList(tagFilter.getText()));
+			}
+			filtered.setPredicate((Product product) -> {
+				if (newV == null || newV.isEmpty()) {
+					return true;
+				}
+
+				if (product.getTags() != null) {
+					for (String tag: tagList) {
+						if (!product.getTags().contains(tag)) {
+							return false;
+						}
+					}
+				} else {
+					return false;
+				}
+				//				if (filtered.contains(product)) {
+				//					return false;
+				//				}
+				return true;
+			});
+		});
+		SortedList<Product> sort = new SortedList<>(filtered);
+		table.setItems(sort);
+		table.skinProperty().addListener(new ResizeColumnsListener(table)); // REFLECTION HACK
 	}
+	
+	public void refresh() {
+		list = FXCollections.observableList(db.getProducts());
+	}
+
+	@Override
+	protected void updateLayout(TabPane tabs) {
+		// 30 is the constant height for the tabs themselves.
+		table.prefHeightProperty().bind(tabs.heightProperty().subtract(30).subtract(filter.getPrefHeight()));
+		table.prefWidthProperty().bind(tabs.widthProperty());
+	}
+	
 	/**
 	 * Creates product using data entered into TextFields
 	 * -ensures price is a valid double value
@@ -48,7 +117,9 @@ public class UpdateProductsController extends Controller {
 		} else if(!StringUtils.isNumber(quantity.getText())) {
 			Dialogs.showDialog(Dialogs.Type.ERROR, "Invalid Quantity Specified", "Quantity must be a valid number.");
 		} else {
-			Product p = database.getProduct(productID.getText());
+			Product p = db.getProduct(productID.getText());
+			if (p == null)
+				Dialogs.showDialog(Dialogs.Type.ERROR, "Invalid Product Specified", "Product ID must be valid.");
 			int qty = Integer.parseInt(quantity.getText());
 			if(qty <= 0) {
 				Dialogs.showDialog(Dialogs.Type.ERROR, "Invalid Quantity Specified", "Quantity must be a positive number.");
@@ -58,7 +129,7 @@ public class UpdateProductsController extends Controller {
 					DatabaseInterface.getInstance().updateItem(p);
 					productID.clear();
 					quantity.clear();
-					((TableView)invView.getChildren().get(1)).refresh();
+					table.refresh();
 				} else {
 					Dialogs.showDialog(Dialogs.Type.ERROR, "Invalid Product ID Specified", String.format("%s is not a valid Product ID.", productID.getText()));
 				}
