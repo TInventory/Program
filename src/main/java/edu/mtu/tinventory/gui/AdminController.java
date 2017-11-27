ade3c6dcd7bcd48dfbe5144fd975315314c11c3a
Parker Young <parker@parker8283.net>
2017-11-27 13:31:19 -0500

    Easter egg for a feature not implemented in time

--- src/main/java/edu/mtu/tinventory/gui/AdminController.java
+++ src/main/java/edu/mtu/tinventory/gui/AdminController.java
@@ -93,7 +93,7 @@ public class AdminController extends Controller {
 
 	@FXML
 	private void changeOverrides() {
-
+		Dialogs.showDialog(Dialogs.Type.ERROR, "This feature has not been implemented.", "Our apologies, but it appears we're out of time.");
 	}
 
 	@FXML
